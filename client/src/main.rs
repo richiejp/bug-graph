5e544af07dc12e247fc1113a89432b549abc250c
Richard Palethorpe <richiejp@f-m.fm>
2018-05-09 21:01:51 +0200

    Partially add websockets

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,26 +1,74 @@
+extern crate failure;
+extern crate stdweb;
 #[macro_use]
 extern crate yew;
 
+use failure::Error;
+use stdweb::web;
 use yew::prelude::*;
+use yew::services::Task;
+use yew::services::websocket::{WebSocketService, WebSocketTask, WebSocketStatus};
 
-struct Context;
-struct Model;
-struct Msg;
+struct Context {
+    ws: WebSocketService,
+}
 
-impl<C> Component<C> for Model {
+impl AsMut<WebSocketService> for Context {
+    fn as_mut(&mut self) -> &mut WebSocketService {
+        &mut self.ws
+    }
+}
+
+struct Model {
+    ws: Option<WebSocketTask>,
+    log: Vec<String>,
+}
+
+enum Msg {
+    Recv(Result<String, Error>),
+    Stat(WebSocketStatus),
+}
+
+impl<C> Component<C> for Model
+where
+    C: AsMut<WebSocketService> + 'static,
+{
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _env: &mut Env<C, Self>) -> Self {
-        Model
+    fn create(_: Self::Properties, env: &mut Env<C, Self>) -> Self {
+        let wss: &mut WebSocketService = env.as_mut();
+        let url = ws_url();
+        let cb = env.send_back(|text| Msg::Recv(text));
+        let evt = env.send_back(|status| Msg::Stat(status));
+        let task = wss.connect(&url, cb, evt);
+
+        Model {
+            ws: Some(task),
+            log: Vec::default(),
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message, _env: &mut Env<C, Self>) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message, env: &mut Env<C, Self>) -> ShouldRender {
+        match msg {
+            Msg::Stat(s) => match s {
+                WebSocketStatus::Opened => self.log.push("Opened websocket".into()),
+                WebSocketStatus::Closed => self.log.push("Closed websocket".into()),
+                WebSocketStatus::Error => self.log.push("Error on websocket".into()),
+            },
+            Msg::Recv(res) => match res {
+                Ok(resp) => self.log.push(format!("Server: {}", resp)),
+                Err(e) => self.log.push(format!("Error receiving: {}", e)),
+            }
+        }
         true
     }
 }
 
-impl<C: 'static> Renderable<C, Model> for Model {
+impl<C> Renderable<C, Model> for Model
+where
+    C: AsMut<WebSocketService> + 'static,
+{
     fn view(&self) -> Html<C, Self> {
         html! {
             <section class="section",>
@@ -28,15 +76,45 @@ impl<C: 'static> Renderable<C, Model> for Model {
                 <h1 class="title",>{ "Bug Graph" }</h1>
                 <p class="subtitle",>{ "Ruining the pychology of bugs everywhere!" }</p>
                 </div>
+                <div class="container",>{ self.render_messages() }</div>
             </section>
         }
     }
 }
 
+impl Model
+{
+    fn render_messages<C>(&self) -> Html<C, Model>
+    where
+        C: AsMut<WebSocketService> + 'static,
+    {
+        self.log.iter()
+            html! {
+                <div class="notification is-info",>{ msg }</div>
+            }
+        }
+    }
+}
+
+fn ws_url() -> String {
+    let loc = web::window().location().expect("Getting host URL");
+    let proto = if "https:" == loc.protocol().expect("Getting connection protocol") {
+        "wss:"
+    } else {
+        "ws:"
+    };
+    let host = loc.host().expect("Getting host");
+
+    format!("{}//{}/ws/", proto, host)
+}
+
 fn main() {
     yew::initialize();
 
-    let app: App<_, Model> = App::new(Context);
+    let app: App<_, Model> = App::new(Context {
+        ws: WebSocketService::new(),
+    });
+
     app.mount_to_body();
     yew::run_loop();
 }
--- src/web.rs
+++ src/web.rs
@@ -1,11 +1,36 @@
 
-//use actix::prelude::*;
+use actix::prelude::*;
 use actix_web::*;
 use actix_web::fs::NamedFile;
 use actix_web::http::Method;
 
+pub struct Ws;
+
+impl Actor for Ws {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        ctx.text("Hello from WS server!");
+    }
+}
+
+impl StreamHandler<ws::Message, ws::ProtocolError> for Ws {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        match msg {
+            ws::Message::Text(text) => {
+                ctx.text(format!("You sent me: {}", text));
+            }
+            _ => ()
+        }
+    }
+}
+
 fn index(_req: HttpRequest) -> Result<NamedFile> {
     Ok(NamedFile::open("res/static/index.html")?)
+}
+
+fn ws_index(req: HttpRequest) -> Result<HttpResponse> {
+    ws::start(req, Ws)
 }
 
 fn static_file(file: Path<String>) -> Result<NamedFile> {
@@ -15,8 +40,8 @@ fn static_file(file: Path<String>) -> Result<NamedFile> {
 pub fn new() -> App<()>
 {
     App::new()
-        .resource("/",
-                  |r| r.method(Method::GET).f(index))
+        .resource("/", |r| r.method(Method::GET).f(index))
+        .resource("/ws/", |r| r.f(ws_index))
         // For now non capture groups (?: ...) confuse the actix-web parser
         // and numbered capture groups confuse the router because they produce
         // surplus matches
